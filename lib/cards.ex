e7036b7be6a377bae6fcd326683f45e934658c75
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-06-06 22:19:03 +0300

    Add fancy methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,59 @@
 defmodule Cards do
+  @moduledoc """
+  Provides methods for cards
+  """
+
+  @doc """
+  Return deck of playing cards
+  """
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+  Divide deck into `hand`
+
+  ### Examples
+  
+  ```elixir
+  deck = Cards.create_deck
+  {hand, deck} = Cards.deal(deck, 1)
+  hand #=> ["Ace of Spades"]
+  ```
+
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    create_deck
+    |> shuffle
+    |> deal(hand_size)
   end
 end
