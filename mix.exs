6bf0f2be992ac4ca76d9b0946e7a04dc1e8bb1a0
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-06-06 23:19:36 +0300

    Add coverage utility

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Cards.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,7 +28,8 @@ defmodule Cards.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-       {:ex_doc, "~> 0.12.0"}
+      {:ex_doc, "~> 0.12.0"},
+      {:excoveralls, "~> 0.8", only: :test}
     ]
   end
 end
